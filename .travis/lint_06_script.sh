cd9a468efc394cf7dba69001a902de8b31a5ef91
harukoxd <harusurv@hotmail.com>
2019-06-07 11:15:03 -0400

    modified:   .travis/lint_06_script.sh

--- .travis/lint_06_script.sh
+++ .travis/lint_06_script.sh
@@ -6,17 +6,17 @@
 
 export LC_ALL=C
 
-if [ "$TRAVIS_EVENT_TYPE" = "pull_request" ]; then
-  test/lint/commit-script-check.sh $TRAVIS_COMMIT_RANGE
-fi
+#if [ "$TRAVIS_EVENT_TYPE" = "pull_request" ]; then
+#  test/lint/commit-script-check.sh $TRAVIS_COMMIT_RANGE
+#fi
 
-test/lint/git-subtree-check.sh src/crypto/ctaes
-test/lint/git-subtree-check.sh src/secp256k1
-test/lint/git-subtree-check.sh src/univalue
-test/lint/git-subtree-check.sh src/leveldb
-test/lint/check-doc.py
-test/lint/check-rpc-mappings.py .
-test/lint/lint-all.sh
+#test/lint/git-subtree-check.sh src/crypto/ctaes
+#test/lint/git-subtree-check.sh src/secp256k1
+#test/lint/git-subtree-check.sh src/univalue
+#test/lint/git-subtree-check.sh src/leveldb
+#test/lint/check-doc.py
+#test/lint/check-rpc-mappings.py .
+#test/lint/lint-all.sh
 
 if [ "$TRAVIS_REPO_SLUG" = "bitcoin/bitcoin" -a "$TRAVIS_EVENT_TYPE" = "cron" ]; then
     git log --merges --before="2 days ago" -1 --format='%H' > ./contrib/verify-commits/trusted-sha512-root-commit
